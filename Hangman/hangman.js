59c4a5c598f5bb26de65707fe9180a460ef147b3
lkarsanb <lkarsanb@uccs.edu>
2025-04-26 18:32:33 -0600

    Add files via upload

---
+++ Hangman/hangman.js
@@ -0,0 +1,158 @@
+import { supabase } from '/helper.js';
+
+//Update user score in database and in right corner.
+async function updateScore() {
+    const { data: { user } } = await supabase.auth.getUser();
+    const statusText = document.getElementById("userStatus");
+
+    //Get user's current score.
+    if (user) {
+        const { data, error } = await supabase
+        .from('Leaderboard')
+        .select('score')
+        .eq('UID', user.id); //Getting score based on user's ID.
+
+        //There was an error getting current score.
+        if (error) {
+            console.error('Error fetching user data:', error);
+        }
+
+        //Otherwise, update the user's score.
+        else {
+            let newScore = data[0].score; 
+            newScore++; //Increment current score.
+
+            //If the user exists, then update their score in the database.
+            if (user) {
+                const { data, error } = await supabase
+                .from('Leaderboard')
+                .update({ score: newScore })
+                .eq('UID', user.id);
+
+                //There was an error updating the score.
+                if (error) {
+                    console.error('Error updating score:', error);
+                }
+            }
+        }
+    }
+}
+
+//Get initial user score from the database.
+async function getScore() {
+    const { data: { user } } = await supabase.auth.getUser();
+    const statusText = document.getElementById("userStatus");
+
+    //If the user exists, then get their score from the database (using their user id).
+    if (user) {
+        const { data, error } = await supabase
+        .from('Leaderboard')
+        .select('score')
+        .eq('UID', user.id);
+
+        //If there was an error, display to console.
+        if (error) {
+            console.error('Error fetching user data:', error);
+
+        //Otherwise, display new score on page.
+        } else {
+            userScore.innerHTML = String(data[0].score);
+        }
+    } else {
+        console.log("No user signed in.");
+        userScore.innerHTML = "0";
+    }
+}
+
+
+const hangmanImage = document.querySelector(".stand img");
+const wordDisplay = document.querySelector(".word-display");
+const guessedText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const gameModel = document.querySelector(".game-model");
+const playAgainBtn = document.querySelector(".play-again");
+const userScore = document.getElementById('user-score');
+
+let currentWord, correctLetters, wrongGuessCount = 0;
+const maxGuesses = 6;
+getScore()
+
+
+// Reset game to allow user to play again.
+const resetGame = () => {
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = `/Hangman/Images/stand.png`
+    guessedText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    getScore();
+    gameModel.classList.remove("show");
+}
+
+// Select a random word from wordList.js.
+const getRandomWord = () => {
+    const {word,hint} = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    document.querySelector(".hint-text b").innerText = hint;
+    resetGame();
+}
+
+// Display something based on if user wins or loses.
+const gameOver = (isVictory) => {
+    setTimeout(() => {
+        const modalText = isVictory ? `You found the word: ` : `The correct word was: `;
+        gameModel.querySelector("img").src = `/Hangman/Images/${isVictory ? 'win.jpg' : 'lost.png'}`;
+        gameModel.querySelector("h4").innerText = `${isVictory ? 'Congrats!' : 'Nice Try!'}`;
+        gameModel.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+        gameModel.classList.add("show");
+    }, 300);
+}
+
+// See if the selected letter is in the word.
+const initGame = (button, clickedLetter) => {
+    // If the letter is in the word, show it in the answer and add letter to list.
+    if(currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+    } 
+
+    // Otherwise, if incorrect letter is guessed, increment incorrect answer count and update hangman image.
+    else {
+        wrongGuessCount++;
+        hangmanImage.src = `/Hangman/Images/${wrongGuessCount}.png`
+    }
+
+    button.disabled = true;
+    guessedText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    // Call gameOver with true or false based on if user guessed the word or not.
+    if (wrongGuessCount === maxGuesses) return gameOver(false);
+    if (correctLetters.length === currentWord.length) {
+        // Increment total score count.
+        updateScore();
+        return gameOver(true);
+    }
+
+
+}
+
+
+// Create the letters buttons.
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+
+    // Add an event when the button is clicked.
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+}
+
+// Generate the random word.
+getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
---
+++ Hangman/words.js
@@ -0,0 +1,102 @@
+const wordList = [
+    {
+        word: "iceberg",
+        hint: "It is ice floating in the sea."
+    },
+    {
+        word: "honest",
+        hint: "It means that you tell the truth."
+    },
+    {
+        word: "green",
+        hint: "It is a color."
+    },
+    {
+        word: "orange",
+        hint: "It is a color."
+    },
+    {
+        word: "purple",
+        hint: "It is a color."
+    },
+    {
+        word: "guitar",
+        hint: "It is a musical instrument."
+    },
+    {
+        word: "piano",
+        hint: "It is a musical instrument."
+    },
+    {
+        word: "trumpet",
+        hint: "It is a musical instrument."
+    },
+    {
+        word: "xylophone",
+        hint: "It is a musical instrument."
+    },
+    {
+        word: "football",
+        hint: "It is a sport."
+    },
+    {
+        word: "soccer",
+        hint: "It is a sport."
+    },
+    {
+        word: "volleyball",
+        hint: "It is a sport."
+    },
+    {
+        word: "chess",
+        hint: "It is a board game."
+    },
+    {
+        word: "rock",
+        hint: "It is a music genre."
+    },
+    {
+        word: "jazz",
+        hint: "It is a music genre."
+    },
+    {
+        word: "pizza",
+        hint: "It is a food."
+    },
+    {
+        word: "pasta",
+        hint: "It is a food."
+    },
+    {
+        word: "hamburger",
+        hint: "It is a food."
+    },
+    {
+        word: "spring",
+        hint: "It is a season."
+    },
+    {
+        word: "summer",
+        hint: "It is a season."
+    },
+    {
+        word: "winter",
+        hint: "It is a season."
+    },
+    {
+        word: "fall",
+        hint: "It is a season."
+    },
+    {
+        word: "january",
+        hint: "It is a month."
+    },
+    {
+        word: "february",
+        hint: "It is a month."
+    },
+    {
+        word: "november",
+        hint: "It is a month."
+    },
+];
